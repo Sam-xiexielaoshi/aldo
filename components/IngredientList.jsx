54061675649b16e0448465a7e29dd0b33f7aa994
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-06-24 12:28:04 +0530

    added components like AldoRecipe and IngreidentsList also modified and cleared up the Main

---
+++ components/IngredientList.jsx
@@ -0,0 +1,23 @@
+export default function IngredientList(props) {
+  const ingredientsListItems = props.ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+  return (
+    <section>
+      
+      <h2>Ingredients on shelf:</h2>
+      <ul className="ingredient-list" aria-live="polite">
+        {ingredientsListItems}
+      </ul>
+      {props.ingredients.length >= 3 ? (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Want a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.toggleRecipe}>Get a recipe</button>
+        </div>
+      ) : null}
+    </section>
+  );
+}
