3d75ab8eac290317eb1704dd44be254716c8b2e3
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-07-13 21:35:28 +0530

    adding useRef for states

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,10 +2,25 @@ import { useState } from "react";
 import AldoRecipe from "./AldoRecipe";
 import IngredientList from "./IngredientList";
 import { getRecipeFromMistral } from "./ai.js";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 export default function Main() {
   const [ingredients, setIngredients] = useState([]);
   const [recipe, setRecipe] = useState("");
+  const recipeStation = useRef(null);
+
+  useEffect(() => {
+    if (recipe !== "" && recipeStation.current !== null) {
+      // recipeStation.current.scrollIntoView();
+      const yCoord =
+        recipeStation.current.getBoundingClientRect().top +
+        window.scroll({
+          top: yCoord,
+          behavior: "smooth",
+        });
+    }
+  }, [recipe]);
 
   async function toggleRecipe() {
     const genertedRecipe = await getRecipeFromMistral(ingredients);
@@ -16,6 +31,7 @@ export default function Main() {
     const newIngredient = formData.get("ingredient");
     setIngredients((prev) => [...prev, newIngredient]);
   }
+
   return (
     <main>
       <form action={addIngredient} className="add-ingredient-form">
@@ -28,9 +44,13 @@ export default function Main() {
         <button>Add ingredient</button>
       </form>
       {ingredients.length > 0 ? (
-        <IngredientList ingredients={ingredients} toggleRecipe={toggleRecipe} />
+        <IngredientList
+          ref={recipeStation}
+          ingredients={ingredients}
+          toggleRecipe={toggleRecipe}
+        />
       ) : null}
-      <section>{recipe ? <AldoRecipe recipe={recipe}/> : null}</section>
+      <section>{recipe ? <AldoRecipe recipe={recipe} /> : null}</section>
     </main>
   );
 }
